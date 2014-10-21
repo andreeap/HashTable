eb2ca46132d283ea098c88f403fc08e5424fb7c6
andreea <andreea.pirvulescu91@gmail.com>
2014-10-21 23:58:31 +0300

    format

--- hash.c
+++ hash.c
@@ -8,12 +8,12 @@
  */
 unsigned int hash(const char *str, unsigned int hash_length)
 {
-	unsigned int hash = 5381;
-	int c;
+    unsigned int hash = 5381;
+    int c;
 
-	while ( (c = *str++) != 0 )
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    while ( (c = *str++) != 0 )
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-	return (hash % hash_length);
+    return (hash % hash_length);
 }
 
--- tema0.c
+++ tema0.c
@@ -1,6 +1,6 @@
 /**
  * HashtTable
- * 
+ *
  * @author Andreea Pirvulescu
  */
 
@@ -28,48 +28,59 @@
  */
 void execute_command(char *buf, HashP ht)
 {
-	char *first, *second, *third;
-	int index;
+    char *first, *second, *third;
+    int index;
 
-	first = strtok(buf, " ");
-	
-	if (strcmp(ADD, first) == 0){
-		second = strtok(NULL, " ");
-		add(second, ht);
-	} 
-	else if (strcmp(RM, first) == 0){
-		second = strtok(NULL, " ");
-		remove_word(second, ht);
-	} 
-	else if (strcmp(CL, first) == 0){
-		clear(ht);
-	} 
-	else if (strcmp(FN, first) == 0){
-		second = strtok(NULL, " ");
-		third = strtok(NULL, " ");
-		find(second, third, ht);
-	} 
-	else if (strcmp(PB, first) == 0){
-		second = strtok(NULL, " ");
-		third = strtok(NULL, " ");
-		sscanf(second, "%d", &index);
-		print_bucket(index, third, ht);
-	} 
-	else if (strcmp(PR, first) == 0){
-		second = strtok(NULL, " ");
-		print(second, ht);
-	} 
-	else if (strcmp(R, first) == 0){
-		second = strtok(NULL, " ");
-		if (strcmp(D, second) == 0){
-			resize_double(ht);
-		} 
-		else if (strcmp(H, second) == 0){
-			resize_halve(ht);
-		}
-	} else {
-		printf("Unrecognized command.");
-	}
+    first = strtok(buf, " ");
+
+    if (strcmp(ADD, first) == 0)
+    {
+        second = strtok(NULL, " ");
+        add(second, ht);
+    }
+    else if (strcmp(RM, first) == 0)
+    {
+        second = strtok(NULL, " ");
+        remove_word(second, ht);
+    }
+    else if (strcmp(CL, first) == 0)
+    {
+        clear(ht);
+    }
+    else if (strcmp(FN, first) == 0)
+    {
+        second = strtok(NULL, " ");
+        third = strtok(NULL, " ");
+        find(second, third, ht);
+    }
+    else if (strcmp(PB, first) == 0)
+    {
+        second = strtok(NULL, " ");
+        third = strtok(NULL, " ");
+        sscanf(second, "%d", &index);
+        print_bucket(index, third, ht);
+    }
+    else if (strcmp(PR, first) == 0)
+    {
+        second = strtok(NULL, " ");
+        print(second, ht);
+    }
+    else if (strcmp(R, first) == 0)
+    {
+        second = strtok(NULL, " ");
+        if (strcmp(D, second) == 0)
+        {
+            resize_double(ht);
+        }
+        else if (strcmp(H, second) == 0)
+        {
+            resize_halve(ht);
+        }
+    }
+    else
+    {
+        printf("Unrecognized command.");
+    }
 }
 
 
@@ -79,40 +90,45 @@ void execute_command(char *buf, HashP ht)
  * @in: file location, number of files, HashP pointer
  * @out: 1-fail, 0-success
  */
-int handle_file(char **names, int n, HashP ht) 
-{	
-	int i;
-	FILE *f;
-	char buffer[20001];
+int handle_file(char **names, int n, HashP ht)
+{
+    int i;
+    FILE *f;
+    char buffer[20001];
 
-	for (i = 0; i < n; i++){
-		f = fopen(names[i],"r");
-		if (!f){
-			printf("Error at opening file %s!\n", names[i]);
-			return 1;
-		} else {
-			while (fscanf(f, "%[^\n]\n)", buffer) == 1){
-				execute_command(buffer, ht);
-			}
-		}
-		
-		fclose(f);
-	}
+    for (i = 0; i < n; i++)
+    {
+        f = fopen(names[i],"r");
+        if (!f)
+        {
+            printf("Error at opening file %s!\n", names[i]);
+            return 1;
+        }
+        else
+        {
+            while (fscanf(f, "%[^\n]\n)", buffer) == 1)
+            {
+                execute_command(buffer, ht);
+            }
+        }
 
-	return 0;
+        fclose(f);
+    }
+
+    return 0;
 }
 
 
 
-int main(int argc, char **argv) 
-{	
-	char **name_files;
-	unsigned int size;
-	int i;
-	HashP hasht;
+int main(int argc, char **argv)
+{
+    char **name_files;
+    unsigned int size;
+    int i;
+    HashP hasht;
 
     if(argc > 2)
-    	name_files = malloc((argc-1) * sizeof(char*));
+        name_files = malloc((argc-1) * sizeof(char*));
 
     for(i = 2; i < argc; i++)
     {
@@ -120,22 +136,23 @@ int main(int argc, char **argv)
     }
 
 
-	size = atoi(argv[1]);
+    size = atoi(argv[1]);
 
-	if(!(hasht = create(size))){
-		fprintf(stderr, "ERROR: create() failed\n");
-	}
+    if(!(hasht = create(size)))
+    {
+        fprintf(stderr, "ERROR: create() failed\n");
+    }
 
-	handle_file(name_files, argc-2, hasht);
+    handle_file(name_files, argc-2, hasht);
 
 
-	for (i = 0; i < argc-2; i++)	
-	{
-		free(name_files[i]);
-	}
+    for (i = 0; i < argc-2; i++)
+    {
+        free(name_files[i]);
+    }
 
-	free(name_files);
-	delete_hashtable(hasht);
+    free(name_files);
+    delete_hashtable(hasht);
 
-	return 0;
+    return 0;
 }
